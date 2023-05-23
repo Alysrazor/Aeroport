9bfaeba93beacfc564e0086d294b346f7e4c472e
Alysrazor <sergiocabades131999@gmail.com>
2023-05-23 12:17:57 +0200

    Update Pista.java
    Añadir el Javadoc correspondiente.

--- src/aeroport/Pista.java
+++ src/aeroport/Pista.java
@@ -1,6 +1,8 @@
 package aeroport;
 
 /**
+ * Clase que representa una {@link Pista} en el {@link Aeroport}
+ *
  * @author Luis Tomas Sahuquilo
  * @dev.main Luis Tomas Sahuquilo
  * @dev.codevs
@@ -8,49 +10,84 @@ package aeroport;
  * @my.company Ciclo Superior de Informática
  */
 public abstract class Pista {
-
-    protected int ID;
-    protected String NombrePista;
-    protected Avion Avion;
+    /**
+    * Identificador de la pista
+    */
+    protected int l_ID;
     
-    public Pista(int ID, String NombrePista, Avion Avion) {
-        this.ID = ID;
-        this.NombrePista = NombrePista;
-        this.Avion = null;
+    /**
+     * Nombre de la {@link Pista}
+     */
+    protected String l_NombrePista;
+    
+    /**
+     * El {@link Avion} que aterriza en la {@link Pista}
+     */
+    protected Avion l_Avion;
+    
+    /**
+     * Constructor por defecto de la {@link Pista}
+     * @param p_ID El identificador de la {@link Pista}
+     * @param p_NombrePista El nombre de la {@link Pista}
+     * @param p_Avion El {@link Avion} que está aterrizando en la {@link Pista}
+     */
+    public Pista(int p_ID, String p_NombrePista, Avion p_Avion) {
+        this.l_ID = p_ID;
+        this.l_NombrePista = p_NombrePista;
+        this.l_Avion = null;
     }
 
-    public int getID() {
-        return ID;
+    /**
+     *  Obtiene la ID de la {@link Pista}
+     *  @return Un {@code int} que es la ID de la {@link Pista}
+     */
+    public int GetID() 
+    {
+        return this.l_ID;
     }
 
-    public String getNombrePista() {
-        return NombrePista;
+    /**
+     *  Obtiene el nombre de la {@link Pista}
+     *  @return Un {@link String} que es el nombre de la {@link Pista}
+     */
+    public String GetNombrePista() 
+    {
+        return this.l_NombrePista;
     }
 
-    public Avion getAvion() {
-        return Avion;
+    /**
+     *  Obtiene el {@link Avion} que está aterrizando en la {@link Pista}
+     *  @return <ul>
+                    <li>Un {@link Avion} que está aterrizando</li>
+                    <li>{@code null} si no hay ningún {@link Avion} aterrizando en la pista</li>
+               </ul>
+     */
+    public Avion GetAvion() 
+    {
+        return this.l_Avion;
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() 
+    {
         int hash = 7;
-        hash = 97 * hash + this.ID;
+        hash = 97 * hash + this.l_ID;
         return hash;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object obj) 
+    {
+        if (this == obj) 
             return true;
-        }
-        if (obj == null) {
+        
+        if (obj == null) 
             return false;
-        }
-        if (getClass() != obj.getClass()) {
+        
+        if (getClass() != obj.getClass()) 
             return false;
-        }
+        
         final Pista other = (Pista) obj;
-        return this.ID == other.ID;
+        return this.l_ID == other.l_ID;
     }
-
 }
