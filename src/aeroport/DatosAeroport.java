b103aa5f14c5bb2db6e67efb5f8a508b7d22bf81
6jmati <6jmati@6jmati>
2023-05-26 09:34:16 +0200

    Actualizar Aeroport

---
+++ src/aeroport/DatosAeroport.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package aeroport;
+
+/**
+ *
+ * @author Sergio Capilla Cabadés
+ * @fecha 26 may. 2023 9:27:43
+ * @company Ciclo Superior de Informática
+ */
+
+public class DatosAeroport 
+{
+    
+}
